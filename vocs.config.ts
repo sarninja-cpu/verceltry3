46bb81d553a4167c894b4f9c62f5e9f7526a756b
Sara Russo <sararusso984@gmail.com>
2025-12-30 12:49:47 +0100

    Update vocs.config.ts
    Signed-off-by: Sara Russo <sararusso984@gmail.com>

--- vocs.config.ts
+++ vocs.config.ts
@@ -14,7 +14,7 @@ const config = {
   logoUrl: 'https://frameworks-static.s3.us-east-2.amazonaws.com/images/logo/frameworks-full.svg',
   iconUrl: '/favicon.ico',
   ogImageUrl: {
-    '/': '/frameworks-twitter.png'
+    '/': 'frameworks-twitter.png'
   },
   sidebar: [
     {
