6348926a5a59e467539634e6aef31d4fb7c40329
Sara Russo <sararusso984@gmail.com>
2026-01-21 14:25:41 +0100

    remove export from config

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vocs'
 import { sidebar } from './components/shared/sidebar'
 
-export const config = {
+const config = {
   banner: {
     content: '***This is a work in progress and not a release. We are looking for volunteers. See [Issues](https://github.com/security-alliance/frameworks/issues) and [Contribution](https://github.com/security-alliance/frameworks/blob/develop/docs/pages/contribute/contributing.mdx) to know how to collaborate.***',
     height: '30px',
