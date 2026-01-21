9e2f075e3e77cee248c9955fa727e98b4e0e0f76
Sara Russo <sararusso984@gmail.com>
2026-01-21 14:22:11 +0100

    try if extracted sidebar works

--- components/overview/FrameworkOverviewFilter.tsx
+++ components/overview/FrameworkOverviewFilter.tsx
@@ -1,5 +1,5 @@
 import React, { Children, isValidElement, ReactNode } from 'react'
-import { config } from '../../vocs.config.ts'
+import { sidebar } from '../shared/sidebar'
 
 const isMainBranch = process.env.CF_PAGES_BRANCH === 'main' || process.env.VERCEL_GIT_COMMIT_REF === 'main'
 
@@ -34,7 +34,7 @@ function getDevOnlyLinks(items: SidebarItem[]): Set<string> {
   return devLinks
 }
 
-const devOnlyLinks = getDevOnlyLinks(config.sidebar as SidebarItem[])
+const devOnlyLinks = getDevOnlyLinks(sidebar as SidebarItem[])
 
 interface ElementProps {
   href?: string
